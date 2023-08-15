8bbbd82ca5573847e006b787d9cef3806940cfa1
Aldin Febriansyah <aldin.20005@mhs.unesa.ac.id>
2023-08-15 22:41:39 +0700

    first commit of fcp-basic-html-css

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+//  File to enable tailwindcss intellisense
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["src/index.html"],
+    theme: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+    },
+    plugins: [],
+  }
